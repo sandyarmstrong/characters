5912bc653dc44c210f71dd65eee4739cfc0107d2
Sandy Armstrong <sandy@xamarin.com>
2018-07-27 10:04:51 -0700

    Disable HTTPS to enable debugging on Mac

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace characters
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
