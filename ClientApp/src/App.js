40583faf0c911d4675264f57ab1516dafaf0c8b7
Sandy Armstrong <sandy@xamarin.com>
2018-07-29 17:40:55 -0700

    Need admin tools

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Character } from './components/Character';
+import { Admin } from './components/Admin';
 
 export default class App extends Component {
   displayName = App.name
@@ -11,6 +12,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Character} />
+        <Route exact path='/admin' component={Admin} />
       </Layout>
     );
   }
---
+++ ClientApp/src/components/Admin.js
@@ -0,0 +1,18 @@
+//@ts-check
+import React, { Component } from 'react';
+import { Badge, Button, Modal } from 'react-bootstrap';
+
+export class Admin extends Component {
+  displayName = Admin.name
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
